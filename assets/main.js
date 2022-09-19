6b4975a3f4f87dca31f9fe7aa9065a63526e139e
nracciatti <nicoracciatti1@gmail.com>
2022-09-19 18:21:36 -0300

    error cuando se ingresa un id que no se reconoce

--- assets/main.js
+++ assets/main.js
@@ -20,16 +20,22 @@ const saveToLocalStorage = () => {
   localStorage.setItem("pizzas", JSON.stringify(pizzas));
 };
 
+const showError = () => {
+  resultado.innerHTML = `<div> 
+    <h2> Ingrese un ID valido. <h2>  <div>`;
+};
+
 //Creo la funcion
 function MostrarPizzas() {
   const dato = parseInt(formulario.value);
   if (dato > 6) {
-    resultado.innerHTML = `<div> 
-    <h2> Ingrese un ID valido. <h2>  <div>`;
+    showError();
     // alert("Ingrese un ID valido.");
     console.log("Ingrese un ID valido.");
     return;
   }
+
+  // Funciona todo correctamente, al poner un ID que no se encuentra se printea el mensaje de que no es valido, el problema es que se queda ahi trabado y no supe como hacer para solucionarlo.
 
   pizzas.forEach((pizza) => {
     if (pizza.id === dato) {
