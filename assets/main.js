ad47bbcff885e845e9e29d7a7204951defe0402d
nracciatti <nicoracciatti1@gmail.com>
2022-09-19 14:10:23 -0300

    hardcodeando img

--- assets/main.js
+++ assets/main.js
@@ -1,35 +1,36 @@
 // Extraigo del DOM.
 
-const pizzaPrice= document.querySelector("#PrecioPizza")
-const pizzaName= document.querySelector("#NombrePizza")
-const pizzaIngredients= document.querySelector("#ingredientesPizza")
-const formulario = document.querySelector("#Formulario")
-const btnBusqueda=document.querySelector("#Boton")
-const resultado = document.querySelector("#Resultado") 
-
+const pizzaPrice = document.querySelector("#PrecioPizza");
+const pizzaName = document.querySelector("#NombrePizza");
+const pizzaIngredients = document.querySelector("#ingredientesPizza");
+const formulario = document.querySelector("#Formulario");
+const btnBusqueda = document.querySelector("#Boton");
+const resultado = document.querySelector("#Resultado");
+const pizzaFoto = document.querySelector("#pizzaFoto");
 
 //escuchador de eventos para que una vez haya cargado por completo el contenido del dom, se ejecute la funcion MostrarPizzas
 
-document.addEventListener('DOMContentLoaded', () =>
-{
-    MostrarPizzas()
-})
+document.addEventListener("DOMContentLoaded", () => {
+  MostrarPizzas();
+});
 
-//Creo la funcion 
-function MostrarPizzas(){
-    
-    const dato= parseInt(formulario.value)
-    if (dato > 6) {
-        alert("Ingrese un ID valido.");
-        console.log("Ingrese un ID valido.");
+//Creo la funcion
+function MostrarPizzas() {
+  const dato = parseInt(formulario.value);
+  if (dato > 6) {
+    resultado.innerHTML = `<div> 
+    <h2> Ingrese un ID valido. <h2>  <div>`;
+    // alert("Ingrese un ID valido.");
+    console.log("Ingrese un ID valido.");
+  }
+  pizzas.forEach((pizza) => {
+    if (pizza.id === dato) {
+      pizzaName.textContent = "Seleccionste: " + `${pizza.nombre}`;
+      pizzaIngredients.textContent = `${pizza.ingredientes}`;
+      pizzaPrice.textContent = `$${pizza.precio}`;
+      pizzaFoto.textContent = `${pizza.foto}`;
     }
-    pizzas.forEach(pizza =>{
-        if(pizza.id === dato){
-            pizzaName.textContent =  "Seleccionaste: " + pizza.nombre
-            pizzaIngredients.textContent = `${pizza.ingredientes}`
-            pizzaPrice.textContent =  `$${pizza.precio}`
-        }
-    })
+  });
 }
 
-btnBusqueda.addEventListener('click', MostrarPizzas)
+btnBusqueda.addEventListener("click", MostrarPizzas);
--- assets/misPizzas.js
+++ assets/misPizzas.js
@@ -1,40 +1,64 @@
 const pizzas = [
-    //Objetos
-    
-    {
-        id: 1,
-        nombre: "Marinara",
-        ingredientes: ["salsa de tomate", " oregano", " ajo", " aceite de oliva", " albahaca"],
-        precio: 450,
-    },
-    {
-        id: 2,
-        nombre: "Margherita",
-        ingredientes: ["Salsa de tomate", " mozzarella", " oregano"," albahaca"],
-        precio: 550,
-    },
-    {
-        id: 3,
-        nombre: "Fugazza",
-        ingredientes: ["Salsa de tomate", " cebolla", " jamon cocido", " oregano", " mozzarella"],
-        precio: 650,
-    },
-    {
-        id: 4,
-        nombre: "Chicago Pizza Style",
-        ingredientes: ["salsa de tomate", " mozzarella", " pepperoni", " salchicha"],
-        precio: 850,
-    },
-    {
-        id: 5,
-        nombre: "Flammkuchen",
-        ingredientes: ["salsa de tomate"," cebolla en aros", " bacon", " rucula"],
-        precio: 900,
-    },
-    {
-        id: 6,
-        nombre: "Sfincione",
-        ingredientes: ["salsa de tomate", " mozzarella", " champiñones", " cebolla cortada", " tomate"],
-        precio: 900,
-    },
-  ]
+  //Objetos
+
+  {
+    id: 1,
+    nombre: "Marinara",
+    ingredientes: [
+      "salsa de tomate",
+      " oregano",
+      " ajo",
+      " aceite de oliva",
+      " albahaca",
+    ],
+    precio: 450,
+  },
+  {
+    id: 2,
+    nombre: "Margherita",
+    ingredientes: ["Salsa de tomate", " mozzarella", " oregano", " albahaca"],
+    precio: 550,
+    foto: "./img/Pizza-Marinara.jpeg",
+  },
+  {
+    id: 3,
+    nombre: "Fugazza",
+    ingredientes: [
+      "Salsa de tomate",
+      " cebolla",
+      " jamon cocido",
+      " oregano",
+      " mozzarella",
+    ],
+    precio: 650,
+  },
+  {
+    id: 4,
+    nombre: "Chicago Pizza Style",
+    ingredientes: [
+      "salsa de tomate",
+      " mozzarella",
+      " pepperoni",
+      " salchicha",
+    ],
+    precio: 850,
+  },
+  {
+    id: 5,
+    nombre: "Flammkuchen",
+    ingredientes: ["salsa de tomate", " cebolla en aros", " bacon", " rucula"],
+    precio: 900,
+  },
+  {
+    id: 6,
+    nombre: "Sfincione",
+    ingredientes: [
+      "salsa de tomate",
+      " mozzarella",
+      " champiñones",
+      " cebolla cortada",
+      " tomate",
+    ],
+    precio: 900,
+  },
+];
